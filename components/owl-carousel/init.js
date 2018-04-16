08ff886f992de6c16a5dcf1326af3483fe666873
alexander <stanislavskiy.sanya@gmail.com>
2018-04-16 18:46:08 +0300

    initial

---
+++ components/owl-carousel/init.js
@@ -0,0 +1,13 @@
+/* INIT SCRIPTS FOR OWL CAROUSEL */
+
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    items: 1,
+    loop: true,
+    autoplay: true,
+    nav: false
+  })
+});
